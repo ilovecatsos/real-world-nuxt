989e58f751df1a98ab7f2d3edfcc02267d6ea983
Kenny Mac <kenneth.macgregor@gmail.com>
2020-09-25 12:12:35 +0100

    meta-seo

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,20 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can create a new event',
+        },
+      ],
+    }
+  },
+}
+</script>
